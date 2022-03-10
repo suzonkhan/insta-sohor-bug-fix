f6dd5f8f3c23bd1dc55f4ada7087d618709e9e6c
Abdur Rahman Suzon <abdur.rahman@egeneration.co>
2022-03-10 20:42:30 +0600

    show user picture

--- js/app.js
+++ js/app.js
@@ -52,7 +52,7 @@ const switchTab = (id) => {
 };
 
 const createPost = (post) => {
-  // console.log(post);
+  console.log(post);
     const image = post.image;
     const div = document.createElement( "article" );
     div.classList.add( "post" );
@@ -64,7 +64,7 @@ const createPost = (post) => {
                     target="_blank"
                     class="post__avatar"
                   >
-                    <img src="${image}" alt="User Picture" />
+                    <img src="${post.userImage}" alt="User Picture" />
                   </a>
                   <a href="#" class="post__user">phero</a>
                 </div>
